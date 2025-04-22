c72f7753b42ccb4d15a60d01da3151ac132b200e
SerraSilvia <152980740+SerraSilvia@users.noreply.github.com>
2025-04-22 08:45:26 +0000

    add a13

---
+++ A13-consumApis/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consum APIS</title>
+</head>
+<body>
+<main>
+        <p><a href="veureTots.php">Veure tots els articles</a></p>
+        <p><a href="veurePremium.php">Veure articles premium</a></p>
+        <p><a href="veurePerData.php">Veure articles a partir d'una data</a></p>
+    </main>
+</body>
+</html>
---
+++ A13-consumApis/veureGratuits.php
@@ -0,0 +1,18 @@
+<?php
+$db = new SQLite3('../database/springfield_news.db');
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && !isset($_REQUEST['visibilitat'])) {
+    $resultats = $db->query("SELECT * FROM articles WHERE art_visibiltat == '' ORDER BY art_id");
+
+    $articles = [];
+
+    while ($row = $resultats->fetchArray(SQLITE3_ASSOC)) {
+        $articles[] = $row;
+    }
+
+    echo json_encode($articles);
+}
+
+$db->close();
+?>
